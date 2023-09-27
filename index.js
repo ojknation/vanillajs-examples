8eb6b1571e6c369210651939938925da79575a84
harrybello14@gmail.com <harrybello14@gmail.com>
2023-09-27 10:34:44 +0100

    create rectangle function added

--- index.js
+++ index.js
@@ -10,3 +10,47 @@ if (rectangleElement) {
 } else {
   console.error('Element with class "rectangle" not found.');
 }
+
+
+const addRectangleButton = document.getElementById("addRectangleButton");
+const rectangleContainer = document.getElementById("rectangleContaner")
+addRectangleButton.addEventListener("click", () => {
+  const rectangle = document.createElement("div");
+  rectangle.classList.add("rectangle");
+  rectangle.style.display = "inline-block";
+  rectangle.style.marginTop = "60px";
+  rectangle.style.marginRight = "10px";
+  rectangleContainer.appendChild(rectangle);
+  if (rectangle) {
+    rectangle.addEventListener("click", () => {
+      color = rectangle.style.backgroundColor;
+      color === "red"
+        ? (rectangle.style.backgroundColor = "blue")
+        : (rectangle.style.backgroundColor = "red");
+    });
+  } else {
+    console.error('Element with class "rectangle" not found.');
+  }
+  
+})
+
+
+// const createRectangle = (parent) => {
+//   const rectangle = document.createElement("div");
+//   rectangle.classList.add("rectangle");
+//   rectangle.style.display = "inline-block";
+//   rectangle.style.marginTop = "60px";
+//   rectangle.style.marginRight = "10px";
+//   parent.appendChild(rectangle);
+//   if (rectangle) {
+//     rectangle.addEventListener("click", () => {
+//       color = rectangle.style.backgroundColor;
+//       color === "red"
+//         ? (rectangle.style.backgroundColor = "blue")
+//         : (rectangle.style.backgroundColor = "red");
+//     });
+//   } else {
+//     console.error('Element with class "rectangle" not found.');
+//   }
+// }
+
