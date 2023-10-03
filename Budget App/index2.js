c8f807e7626f03576144ce8ed87ae2fa6884818f
harrybello14@gmail.com <harrybello14@gmail.com>
2023-10-03 11:33:34 +0100

    input field for expense and income added

--- Budget App/index2.js
+++ Budget App/index2.js
@@ -5,6 +5,7 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
+const select = document.getElementById("option")
 
 // Get transaction From Local Storage
 const localStorageTransactions = JSON.parse(
@@ -17,19 +18,34 @@ let transactions = localStorage.getItem("transactions") !== null ? localStorageT
 //  Add Transactions
 const addTransaction = (event) => {
   event.preventDefault();
-  if (text.value.trim() === "" || amount.value.trim() === "") {
-    document.getElementById("error_msg").innerHTML = "<span>Error: Please enter description and amount</span>";
+  if (text.value.trim() === "" || amount.value.trim() === "" || select.value === "") {
+    document.getElementById("error_msg").innerHTML = "<span>Error: Please enter Description, Amount and choose your Transaction Type</span>";
     setTimeout(
       () => (document.getElementById("error_msg").innerHTML= ""),
       5000
     );
   } else {
-    const transaction = {
+    let transaction = {
       id: genrateID(),
       text: text.value,
-      amount: +amount.value,
+      amount: amount.value,
+      type: select.value,
     };
-
+    if (select.value === "income"){
+      transaction = {
+        id: genrateID(),
+        text: text.value,
+        amount: +amount.value,
+        type: select.value,
+      };
+    }else{
+      transaction = {
+        id: genrateID(),
+        text: text.value,
+        amount: -amount.value,
+        type: select.value,
+      };
+    }
     transactions.push(transaction);
 
     addTransactionDOM(transaction);
@@ -40,6 +56,8 @@ const addTransaction = (event) => {
     
     text.value = "";
     amount.value = "";
+    select.value = "";
+    
   }
 }
 
@@ -58,7 +76,7 @@ const addTransactionDOM = (transaction) => {
   item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
   item.innerHTML = `
-  ${transaction.text} ${sign}${Math.abs(transaction.amount)}
+  ${transaction.text} (${transaction.type}) ${sign}${Math.abs(transaction.amount)}
   <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`;
 
   list.appendChild(item);
@@ -67,8 +85,8 @@ const addTransactionDOM = (transaction) => {
 
 // update the balance,inflow and outflow
 const updateValues = () => {
+  const types = transactions.filter((transaction) => transaction.type)
   const amounts = transactions.map((transaction) => transaction.amount);
-
   const total = amounts.reduce((bal, value) => (bal += value), 0).toFixed(2);
 
   const income = amounts
@@ -80,7 +98,7 @@ const updateValues = () => {
    .filter((value) => value < 0)
    .reduce((bal, value) => (bal += value),0) * -(1)
    .toFixed(2);
-
+ 
   balance.innerText = `$${total}`;
   inflow.innerText = `$${income}`;
   outflow.innerText = `$${expense}`;
